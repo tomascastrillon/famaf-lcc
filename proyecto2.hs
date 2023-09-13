5c00c31ab7ddb6e5b80304eab9c684b529fb8608
tomascastrillon <tomasacastrillon@gmail.com>
2023-09-12 23:35:52 -0300

    ej 8

--- proyecto2.hs
+++ proyecto2.hs
@@ -50,19 +50,19 @@ minimoElemento' (x:y:xs) | x<y = minimoElemento' (x:xs)
 type Altura = Int
 type NumCamiseta = Int
 -- Tipos algebraicos sin parametros (aka enumerados)
-data Zona = Arco | Defensa | Mediocampo | Delantera deriving (Eq)
-data TipoReves = DosManos | UnaMano
-data Modalidad = Carretera | Pista | Monte | BMX
-data PiernaHabil = Izquierda | Derecha
+data Zona = Arco | Defensa | Mediocampo | Delantera deriving (Eq,Show,Ord)
+data TipoReves = DosManos | UnaMano deriving (Eq,Show,Ord)
+data Modalidad = Carretera | Pista | Monte | BMX deriving (Eq,Show,Ord)
+data PiernaHabil = Izquierda | Derecha deriving (Eq,Show,Ord)
 --Sinonimo
 type ManoHabil = PiernaHabil
 --Deportista es un tipo algebraico con constructores parametricos
 data Deportista = Ajedrecista --Constructor sin argumentos
                  | Ciclista Modalidad --Constructor con un argumento
                  | Velocista Altura --Constructor con un argumento
-                 | Tenista TipoReves ManoHabil Altura --Constructor con tres argumentos
-                 | Futbolista Zona NumCamiseta PiernaHabil Altura --Constructor con cuatro argumentos
-
+                 | Tenista TipoReves ManoHabil Altura 
+                 | Futbolista Zona NumCamiseta PiernaHabil Altura 
+                deriving (Show)
 --Apartado b)
 --ghci> :t Ciclista 
 --Ciclista :: Modalidad -> Deportista
@@ -76,8 +76,11 @@ contar_velocistas (x:xs) = contar_velocistas xs
 --Apartado d)
 contar_futbolistas :: [Deportista] -> Zona -> Int
 contar_futbolistas [] z = 0
-contar_futbolistas ((Futbolista zn n p a):xs) z | (z==zn) = 1 + contar_futbolistas xs z -- Corregir
-                                                | otherwise = contar_futbolistas xs z
+contar_futbolistas ((Futbolista Arco n p a):xs) Arco = 1+contar_futbolistas xs Arco
+contar_futbolistas ((Futbolista Defensa n p a):xs) Defensa = 1+contar_futbolistas xs Defensa
+contar_futbolistas ((Futbolista Mediocampo n p a):xs) Mediocampo = 1+contar_futbolistas xs Mediocampo
+contar_futbolistas ((Futbolista Delantera n p a):xs) Delantera = 1+contar_futbolistas xs Delantera
+contar_futbolistas (_:xs) zn = contar_futbolistas xs zn
 
 --Apartado e)
 contar_futbolistas' :: [Deportista] -> Zona -> Int
@@ -128,11 +131,71 @@ primerElemento xs = Just(head xs)
 
 --Ejercicio 7
 data Cola = VaciaC | Encolada Deportista Cola 
+             deriving (Show)
 --Apartado a)
+--1
 atender :: Cola -> Maybe Cola
 atender VaciaC = Nothing
-atender (Encolada d c) = Just c 
+atender (Encolada _ c) = Just c 
+
+--2
+encolar :: Deportista -> Cola -> Cola
+encolar dp VaciaC = Encolada dp VaciaC
+encolar dp (Encolada d y) = Encolada d (encolar dp y)
+
+--3
+busca :: Cola -> Zona -> Maybe Deportista
+busca VaciaC _ = Nothing
+busca (Encolada (Futbolista z n p a) c) zn   | z==zn = Just (Futbolista z n p a)    
+                                             | otherwise = busca c zn
+busca (Encolada dp c) zn = busca c zn  
 
 --Apartado b)
-encolar :: Deportista -> Cola -> Cola
-encolar dp c = Encolada dp 
+--Las Colas se parecen a las listas ya que se puede hacer recursion sobre ellas, la diferencia es que son FIFO(First In, First Out).
+
+--Ejercicio 8
+data ListaAsoc a b = Vacia | Nodo a b (ListaAsoc a b) deriving(Show)
+
+--Apartado a)
+type Telefonica = ListaAsoc String Int
+
+--Apartado b)
+--1
+la_long :: ListaAsoc a b -> Int
+la_long Vacia = 0
+la_long (Nodo x y ls) = 1+la_long ls
+
+--2 
+la_concat :: ListaAsoc a b -> ListaAsoc a b -> ListaAsoc a b 
+la_concat Vacia Vacia = Vacia
+la_concat Vacia ls1 = ls1
+la_concat (Nodo x y ls) ls1 = Nodo x y (la_concat ls ls1)
+
+--3
+la_agregar :: Eq a => ListaAsoc a b -> a -> b -> ListaAsoc a b
+la_agregar Vacia x y = (Nodo x y Vacia)
+la_agregar (Nodo c v la) x y | c==x = (Nodo c y) la
+                             | otherwise = Nodo c v (la_agregar la x y)
+                             
+--4 
+la_pares :: ListaAsoc a b -> [(a,b)]
+la_pares Vacia = []
+la_pares (Nodo x y ls) = (x,y): la_pares ls
+
+--5
+la_busca :: Eq a => ListaAsoc a b -> a -> Maybe b
+la_busca Vacia c = Nothing
+la_busca (Nodo x y ls) c | x==c = Just y
+                         | otherwise = la_busca ls c
+
+--6
+--Borra la primera que encuentra
+la_borrar :: Eq a => a -> ListaAsoc a b -> ListaAsoc a b
+la_borrar c Vacia = Vacia
+la_borrar c (Nodo x y ls) | c == x = ls
+                          | otherwise = Nodo x y (la_borrar  c ls)
+--Borra todas las que encuentra                
+la_borrar' :: Eq a => a -> ListaAsoc a b -> ListaAsoc a b
+la_borrar' c Vacia = Vacia
+la_borrar' c (Nodo x y ls) | c == x = la_borrar' c ls
+                          | otherwise = Nodo x y (la_borrar'  c ls)
